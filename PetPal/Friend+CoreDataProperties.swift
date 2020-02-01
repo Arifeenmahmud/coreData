fca81e1265ecf446e0eaa4645bf0d6057143bdda
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2020-02-01 07:13:47 -0800

    Initial Commit

---
+++ PetPal/Friend+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Friend+CoreDataProperties.swift
+//  PetPal
+//
+//  Created by Lab on 1/31/20.
+//  Copyright © 2020 Razeware. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Friend {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Friend> {
+        return NSFetchRequest<Friend>(entityName: "Friend")
+    }
+
+    @NSManaged public var name: String?
+
+}
