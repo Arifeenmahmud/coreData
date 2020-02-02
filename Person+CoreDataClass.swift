9416c6a95f07a3e1d58ce4af14cd7b3a2e7139d2
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2020-02-03 00:02:10 +0600

    Kilo Loco

---
+++ Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  CoreDataDevices
+//
+//  Created by Arifeen Mahmud on 2/2/20.
+//  Copyright © 2020 Arifeen Mahmud. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Person: NSManagedObject {
+
+}
