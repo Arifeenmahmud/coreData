8330652390a91b93d1bd3485c12aba98a2e64201
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2020-02-02 17:09:27 +0600

    Initial Commit

---
+++ CoreDataDevices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataDevices
+//
+//  Created by Arifeen Mahmud on 2/2/20.
+//  Copyright © 2020 Arifeen Mahmud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
